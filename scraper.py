246ccc276e662ca6b0a651aafe4dcf6c3771465d
Korneel Dumon <korneel.dumon@cs.kuleuven.be>
2015-09-10 21:29:40 +0200

    new threadpool implementation

--- scraper.py
+++ scraper.py
@@ -3,93 +3,9 @@ import flickr_scraper
 from PIL import Image
 import scipy, threading, time
 
-class FetcherThread(threading.Thread):
-    def __init__(self, threadID, fetcher, result_buf, result_lock):
-        threading.Thread.__init__(self)
-        self.my_id = threadID
-        self.fetcher = fetcher
-        self.results = result_buf
-        self.results_lock = result_lock
-        self.url = None
-        self.urlLock = threading.Condition()
-        self.stopped = False
-    
-    def stopThread(self):
-        self.stopped = True
-        self.urlLock.acquire()
-        self.urlLock.notify()
-        self.urlLock.release()
-    
-    def getUrl(self):
-        return self.url
-    
-    def setUrl(self, url):
-        self.urlLock.acquire()
-        self.url = url
-        self.urlLock.notify()
-        self.urlLock.release()
-    
-    def run(self):
-        while (not self.stopped):
-            self.urlLock.acquire()
-            while (self.url == None):
-                if (self.stopped):
-                    return
-                self.urlLock.wait()
-            # print "Thread %d : downloading %s" % (self.my_id, self.url[-30:])
-            data = self.fetcher(self.url)
-            if (data == None):
-                continue
-            self.results_lock.acquire()
-            self.results.append(data)
-            self.results_lock.release()
-            # print "Thread %d : finished downloading" % (self.my_id)
-            self.url = None
-            self.urlLock.release()
-        return
+from ScraperPool import *
 
 
-class FetcherPool(object):
-    def __init__(self, fetcher, urls, poolsize):
-        self.fetcher = fetcher
-        self.urls = urls
-        self.nbthreads = poolsize
-        if (len(self.urls) < self.nbthreads):
-            self.nbthreads = len(self.urls)
-        self.results = []
-        self.res_lock = threading.Lock()
-        self.threads = []
-        for i in range(self.nbthreads):
-            tic = time.time()
-            t = FetcherThread(i, self.fetcher, self.results, self.res_lock)
-            t.start()
-            self.threads.append(t)
-    
-    def getFreeThread(self):
-        for t in self.threads:
-            if (t.getUrl() == None):
-                return t
-        return None
-
-    def freeThreads(self):
-        while len(self.threads) != 0:
-            self.threads[0].stopThread()
-            self.threads[0].join()
-            self.threads.pop(0)
-    
-    def fetchUrls(self):
-        while (len(self.urls) != 0):
-            url = self.urls[0]
-            t = self.getFreeThread()
-            if (t != None):
-                t.setUrl(url)
-                self.urls.pop(0)
-                #print "Downloading: %d left ..." % (len(self.urls))
-        print "Cleaning up threads ..."
-        self.freeThreads()
-        print "Deleted all download threads"
-        return self.results
-    
 def process(pars):
     NPlacers = pars['NPlacers']
     NScrapers = pars['NScrapers']
@@ -118,7 +34,8 @@ def process(pars):
         poolsize = 20
         fp = FetcherPool(fs.fetchFileData, urls[rank-1 : per_page : NScrapers],
                          poolsize)
-        arrs = fp.fetchUrls()
+        # arrs = fp.fetchUrls()
+        arrs = fp.executeJobs()
         ids = page*per_page + scipy.arange(rank-1, per_page,  NScrapers, dtype=int)
         print "files fetched for page {}".format(page)
         Compacts = []
