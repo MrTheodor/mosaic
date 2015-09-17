6a602d42170a78e15f2f21fa7bcd7b2eda89bac0
KeithWM <keith@myerscough.nl>
2015-09-17 12:19:58 +0200

    changed calling scripts

--- mosaic.sh
+++ mosaic.sh
@@ -1,1 +1,1 @@
-mpiexec -np 3 -timestamp-output python main.py NScrapers 1 NPlacers 1 per_page 10 MaxTilesVert 3&> log
+mpiexec -np 5 -timestamp-output python main.py NScrapers 2 NPlacers 2 per_page 10 MaxTilesVert 4 iters 2 fidelity 5 &> log
