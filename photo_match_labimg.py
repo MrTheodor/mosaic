73a53c36779a7c1e8018785ee624e5e9928a8441
KeithWM <keith@myerscough.nl>
2015-09-23 01:22:43 +0200

    Fixed an error in the compactRepresentation due to passing 4D arrays

--- photo_match_labimg.py
+++ photo_match_labimg.py
@@ -13,7 +13,7 @@ from skimage import color
 class photoMatch(object):
     
     def __init__(self, par={'fidelity': 5}):
-        print "Initializing an instance of the photo match that uses the L2 distance between two arrays as its metric"
+        print "Initializing an instance of the photo match that uses the L2 distance between two arrays in Lab colour space as its metric"
         self.type = 'Tiny image, comparing mean'
         N = par['fidelity']
         self.N = N
@@ -27,10 +27,13 @@ class photoMatch(object):
     # baasically a coarse graining
     def compactRepresentation(self, arrs):
         N = self.N
-        arrs_subsampled = scipy.zeros((arrs.shape[0], N,N, 3))
-        for i in range(arrs.shape[0]):
-            arrs_subsampled[i,...] = scipy.misc.imresize(arrs[i], (N,N))
-        return color.rgb2lab(arrs_subsampled)
+        if len(arrs.shape) == 4:
+            result = scipy.zeros((arrs.shape[0], N,N, 3))
+            for i in range(arrs.shape[0]):
+                result[i,...] = color.rgb2lab(scipy.misc.imresize(arrs[i], (N,N)))
+        elif len(arrs.shape) == 3: # arrs is in fact just arr
+            result = color.rgb2lab(scipy.misc.imresize(arrs, (N,N))).reshape(1,N,N,3)
+        return result
         
     # provide some distance between two compact representations of photos
     # if photo1==photo2, distance should ideally be zeros
@@ -39,7 +42,41 @@ class photoMatch(object):
         #compare the candidates to the target accordin to some measure
         targetarr = scipy.array(target.reshape((self.totalSize/3, 3)), dtype=int)
         candidatesarr = scipy.array(candidates.reshape((candidates.shape[0], self.totalSize/3, 3)), dtype=int)
+        print scipy.sum((targetarr - candidatesarr)**2*self.weights, axis=1)
         return scipy.sum((targetarr - candidatesarr)**2*self.weights, axis=(1,2))
         
     def formatOutput(self, arr):
         return arr
+
+if __name__ == "__main__":
+    from matplotlib import pyplot as plt
+    from PIL import Image
+    plt.close('all')
+    
+    pm = photoMatch({'fidelity' : 5})
+    
+    TargetArr = scipy.zeros((100, 100, 3), dtype='uint8')
+    TargetArr[...,2] = scipy.arange(0,100,1)
+    TargetArr[...,1] = TargetArr[...,2].T
+    TargetCompact = pm.compactRepresentation(TargetArr)
+    
+    K = 5
+    CandArrs = scipy.ones((K,100,100,3), dtype='uint8')
+#    CandArrs[...] = TargetArr
+    for k in range(K):
+        CandArrs[k,...]*= k*(255/10)
+    CandCompacts = pm.compactRepresentation(CandArrs)
+    
+    plt.figure()
+    plt.imshow(TargetArr, interpolation='None')
+    
+    plt.figure()
+    plt.imshow(color.lab2rgb(TargetCompact[0,...]), interpolation='None')
+    
+    plt.figure()
+    plt.imshow(CandArrs[4,...], interpolation='None')
+    
+    plt.figure()
+    plt.imshow(color.lab2rgb(CandCompacts[4,...]), interpolation='None')
+    
+    dist = pm.compactDistance(TargetCompact, CandCompacts)
